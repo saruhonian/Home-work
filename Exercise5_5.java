00be61864cd5bace53843c693580c8a09f5b0148
saruhonian <saruhonianartem@gmail.com>
2016-03-28 17:00:23 +0300

    Added files via upload

---
+++ Exercise5_5.java
@@ -0,0 +1,22 @@
+package ua.od.hillel.HomeWork;
+
+public class Exercise5_5 {
+    public static void main(String[] args) {
+        int num1 = 15;
+        int num2 = 5;
+
+        int sum = num1 % num2;
+
+
+        if (sum == 0)        {
+            System.out.println("da");
+
+        } else
+        System.out.println("net");
+
+
+
+
+
+    }
+}
